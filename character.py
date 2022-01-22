260fad40425062836b157588af0e39966354d32e
KrabikKoastya <kostya.krabik.sw@gmail.com>
2022-01-22 15:35:03 +0200

    Test

---
+++ character.py
@@ -0,0 +1,42 @@
+from abc import abstractmethod, ABC
+
+
+class Fild:
+    melleRow = []
+    rangedRow = []
+    
+
+
+class Character(ABC):
+    inventory = []
+    hp = 0
+    attackDNG = 0
+    spavn = ""
+
+    def move():
+        pass
+
+    def attack():
+        pass
+
+    def desision():
+        pass
+
+    def look():
+        pass
+
+
+class People(Character):
+
+    maxAge = 80
+    skills = []
+    name = ""
+    origin = "Human"
+    fertility = True
+    xenophobia = True
+
+    def walk():
+
+        return "walk"
+
+
