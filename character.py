b5e855fb24fb74ec0b0d5076d59d7ec0fd3f76d6
purumpu <romkaphotographer@gmail.com>
2022-01-26 18:59:34 +0200

    0 COMMIT.

--- character.py
+++ character.py
@@ -1,44 +1,88 @@
 from abc import abstractmethod, ABC
 
 
-class Fild:
+class Field:
     melleRow = []
     rangedRow = []
-    
 
 
-class Character(ABC):
-    inventory = []
-    hp = 0
-    attackDNG = 0
-    spavn = ""
+class Character():
+    def __init__(self, skills, name, inventory, hp, attackDMG, spawn):
+        self.skills = []
+        self.name = ""
+        self.inventory = []
+        self.hp = 0
+        self.attackDMG = 0
+        self.spawn = ""
 
+    @abstractmethod
     def move():
         pass
 
+    @abstractmethod
     def attack():
         pass
 
+    @abstractmethod
     def desision():
         pass
 
+    @abstractmethod
     def look():
         pass
 
 
-class People(Character):
+class Aen_elle(Character):
+    pass
 
-    maxAge = 80
-    skills = []
-    name = ""
-    origin = "Human"
-    fertility = True
-    xenophobia = True
+
+class Dworf(Character):
+    pass
+
+
+class Human(Character):
+    def __init__(self, skills, name, inventory, hp, attackDMG, spawn, maxAge, origin, fertility, xenofobia):
+        super().__init__(skills, name, inventory, hp, attackDMG, spawn)
+        self.maxAge = 80
+        self.origin = "Human"
+        self.fertility = True
+        self.xenophobia = True
 
     def walk():
-
         return "walk"
 
 
-# smth new
+class Witcher(Human):
+    def __init__(self, skills, name, inventory, hp, attackDMG, spawn, maxAge, origin, fertility, xenofobia):
+        super().__init__(skills, name, inventory, hp, attackDMG, spawn, maxAge, origin, fertility, xenofobia)
+        self.hp = 200
 
+
+class Wizard(Human):
+    pass
+
+
+geralt = Witcher()
+print(geralt.hp)
+
+# Gervant geralt
+# Excel eskel
+# harmful dick A.K.A. lambert lam
+# Vasiamyr ves
+# Letov-from-kotletov let
+# Siri ciri
+# Yep yen
+# Triss tris
+# Keira Merz keira
+# Mousevour mysh
+# Roshen roshe
+# Hlamar hlam
+#
+#
+# Eredine ere
+# Imleryh iml
+# Karantir car
+# Avallak'h ava
+#
+#
+# goldy zol
